86237329d5774b194ca680fd96ad9dcd379e39a3
Paul Montes <paulmnts20@gmail.com>
2024-08-25 16:18:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//Variables//
+
+const textArea = document.querySelector(".input");
+const mensaje = document.querySelector(".output");
+
+
+//logica de encriptación//
+
+/*Las "llaves" de encriptación:
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+
+}
+
+function copyText() {
+    let output = document.getElementById("output");
+    
+    output.select();
+    output.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(output.value);
+
+}
+
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+  
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+}
+
